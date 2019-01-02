c5cd6409b62b51b7006ef0f43c236e156f132172
syahroelanwar <34033084+syahroelanwar@users.noreply.github.com>
2019-01-02 10:54:10 +0700

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,79 @@
+const staticCacheName = 'cache-v1';
+const filesToCache = [
+  '/',
+  'index.html',
+  'detail.html',
+  'about.html',
+  'contact.html',
+  'pages/page-offline.html',
+  'pages/page-404.html',
+  'css/bootstrap.min.css',
+  'css/style.css',
+  'js/main.js',
+  'js/detail.js',
+  'js/jquery.min.js',
+  'js/tether.min.js',
+  'js/bootstrap.min.js',
+  'img/classic-pattern-bg.png',
+  'img/notfound.png',
+  'img/map.jpg',
+  'img/ftr.jpg',
+  'img/offline.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+      .then(response => {
+        if (response.status === 404) {
+          return caches.match('pages/page-404.html');
+        }
+        return caches.open(staticCacheName)
+        .then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+    }).catch(error => {
+      console.log('Error, ', error);
+      return caches.match('pages/page-offline.html');
+    })
+  );
+});
